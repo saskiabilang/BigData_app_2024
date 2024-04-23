527477249f30a6af0c4fd185053fbda757afb0cf
saskiabilang <167295695+saskiabilang@users.noreply.github.com>
2024-04-23 14:41:30 +0200

    Add files via upload
    Das ist die erste Version der Applikation

---
+++ compass.app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 13:12:17 2024
+
+@author: Saskia Bilang
+"""
+
+import streamlit as st
+st.set_page_config(
+    page_title = "Modernization Compass",
+    page_icon = "🏡", 
+    layout = "wide")
+
+st.header ("Modernization Compass")
+st.write("Dear User", 
+         "This is a compass to give you guidelines about the modernization potential of your home")
